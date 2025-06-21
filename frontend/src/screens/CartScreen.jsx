00d74c76309b33a69846e5257791ee12dbefee3d
Divya-100701 <diviyadgl2001@gmail.com>
2025-06-21 17:20:22 +0530

    Changing place order

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,110 +1,145 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { Row, Col, ListGroup, Image, Form ,Card, Button } from 'react-bootstrap';
-import { FaTrash } from 'react-icons/fa';
-import Message from '../components/Message';
-import { addToCart, removeFromCart } from '../slices/cartSlice';
+import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Card,
+  Button,
+} from "react-bootstrap";
+import { FaTrash } from "react-icons/fa";
+import Message from "../components/Message";
+import { addToCart, removeFromCart } from "../slices/cartSlice";
 
 const CartScreen = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
 
-    const cart = useSelector((state) => state.cart);
-    const { cartItems } = cart;
+  const [showAlert, setShowAlert] = useState(false);
 
-    const addToCartHandler = async (product,qty) => {
-        dispatch(addToCart({ ...product, qty }));
-        
-    }
-    const removeFromCartHandler = async (id) => {
-        dispatch(removeFromCart(id));
-        
-    }
-    const checkoutHandler = () => {
-        navigate('/login?redirect=/shipping');
-    }
+  const addToCartHandler = async (product, qty) => {
+    dispatch(addToCart({ ...product, qty }));
+  };
+  const removeFromCartHandler = async (id) => {
+    dispatch(removeFromCart(id));
+  };
+
+  const checkoutHandler = () => {
+    setShowAlert(true); // Show the custom alert
+  };
+
+  const handleAlertConfirm = () => {
+    setShowAlert(false); // Hide alert
+    navigate("/");
+  };
 
   return (
     <Row>
-        <Col md={8}>
-            <h1 style={{marginBottom: '20px'}}> Shopping Cart </h1>
-            { cartItems.length === 0 ? (
-                <Message>
-                    Your cart is empty <Link to="/">Go Back</Link>
-                </Message>
-            ) : (
-                <ListGroup variant='flush'>
-                    {cartItems.map((item) => (
-                        <ListGroup.Item key={item._id}>
-                            <Row>
-                                <Col md={2}>
-                                    <Image src={item.image} alt={item.name} fluid rounded />
-                                </Col>
-                                <Col md={3}>
-                                    <Link to={`/product/${item._id}`}>{item.name}</Link>
-                                </Col>
-                                <Col md={2}>
-                                    ${item.price}
-                                </Col>
-                                <Col md={2}>
-                                    <Form.Control 
-                                        as='select' 
-                                        value={item.qty} 
-                                        onChange={(e) => addToCartHandler(item, Number(e.target.value))}
-                                    >
-                                        {[...Array(item.countInStock).keys()].map(x => (
-                                            <option key={x + 1} value={x + 1}>
-                                                {x + 1}
-                                            </option>
-                                        ))}
-                                    </Form.Control>
-                                </Col>
-                                <Col md={2}>
-                                    <Button 
-                                        type='button' 
-                                        variant='light' 
-                                        onClick={() => {removeFromCartHandler(item._id)}}
-                                    >
-                                        <FaTrash />
-                                    </Button>
-                                </Col>
-                            </Row>
-                        </ListGroup.Item>
-                    ))}
-
-                </ListGroup>
-                    
-
-            )
-            }
-        </Col>
-        <Col md={4}>
-            <Card>
-                <ListGroup variant='flush'>
-                    <ListGroup.Item>
-                        <h2>
-                            Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)}) items
-                        </h2>
-                        ${cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                        <Button 
-                            type='button' 
-                            className='btn-block' 
-                            disabled={cartItems.length === 0}
-                            onClick={checkoutHandler}
-                        >
-                            Proceed to Checkout
-                        </Button>
-                    </ListGroup.Item>
-                </ListGroup>
-            </Card>
-        </Col>
-
+      <Col md={8}>
+        <h1 style={{ marginBottom: "20px" }}> Shopping Cart </h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Your cart is empty <Link to="/">Go Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant="flush">
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item._id}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item._id}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>
+                    <Form.Control
+                      as="select"
+                      value={item.qty}
+                      onChange={(e) =>
+                        addToCartHandler(item, Number(e.target.value))
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={2}>
+                    <Button
+                      type="button"
+                      variant="light"
+                      onClick={() => {
+                        removeFromCartHandler(item._id);
+                      }}
+                    >
+                      <FaTrash />
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>
+                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                items
+              </h2>
+              $
+              {cartItems
+                .reduce((acc, item) => acc + item.qty * item.price, 0)
+                .toFixed(2)}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                type="button"
+                className="btn-block"
+                disabled={cartItems.length === 0}
+                onClick={checkoutHandler}
+              >
+                Place Order
+              </Button>
+              {showAlert && (
+                <div
+                  style={{
+                    position: "fixed",
+                    top: "30%",
+                    left: "50%",
+                    transform: "translateX(-50%)",
+                    background: "white",
+                    padding: "20px",
+                    boxShadow: "0 0 10px rgba(0,0,0,0.3)",
+                    borderRadius: "8px",
+                    zIndex: 1000,
+                    width: "300px",
+                    textAlign: "center",
+                  }}
+                >
+                  <p>Your Order has been placed successfully</p>
+                  <button onClick={handleAlertConfirm}>OK</button>
+                </div>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
     </Row>
-  )
-}
+  );
+};
 
-export default CartScreen
+export default CartScreen;
